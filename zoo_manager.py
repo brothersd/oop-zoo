9b5a47868e5b41438ab3e380e7c288983811070f
Dan Brothers <brothersd@hotmail.com>
2025-11-22 10:11:02 -0700

    Updated zoo manager code

---
+++ zoo_manager.py
@@ -0,0 +1,58 @@
+class Animal:
+    def __init__(self, name, species):
+        self.name = name
+        self.species = species
+
+    def speak(self):
+        return("Animal sound")
+
+class Mammal(Animal):
+    def give_birth(self):
+        return(f"{self.name} the {self.species} has given birth")
+
+class Bird(Animal):
+    def __init__(self, name, species, wingspan):
+        super().__init__(name, species)
+        self.wingspan = wingspan
+
+class Reptile(Animal):
+    def bask_in_sun(self):
+        return(f"{self.name} the {self.species} is basking in the sun")
+
+class Primate(Mammal):
+    def climb_trees(self):
+        return(f"{self.name} the {self.species} is climbing trees")
+
+class Marsupial(Mammal):
+    def carry_baby(self):
+        return(f"{self.name} the {self.species} is carrying its baby")
+
+class Aviary:
+    def __init__(self):
+        self.birds = []
+
+class ReptileEnclosure:
+    def __init__(self):
+        self.reptiles = []
+
+
+
+animal = Animal("Lion", "Felis leo")
+print(animal.name)
+print(animal.species)
+print(animal.speak())
+
+mammal = Mammal("Giraffe", "Giraffa camelopardalis")
+print(mammal.give_birth())
+        
+bird = Bird("Eagle", "Aquila chrysaetos", wingspan=2.5)
+print(bird.wingspan)
+
+reptile = Reptile("Turtle", "Testudines")
+print(reptile.bask_in_sun())
+
+primate = Primate("Chimpanzee", "Pan troglodytes")
+print(primate.climb_trees())
+
+marsupial = Marsupial("Kangaroo", "Macropus")
+print(marsupial.carry_baby())
